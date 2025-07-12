92c4caf1f494c127f77b2513326b90f854e88042
AmypDV <amypdv@yandex.ru>
2025-07-12 22:43:54 +0300

    Добавил проверку на наличие bd, а при отсутствии его создание

--- datebase/datebase.py
+++ datebase/datebase.py
@@ -25,14 +25,29 @@ def write_to_bd() -> None:
         pickle.dump(users_bd, file)
     logger.info('Запись в БД')
 
+
 def read_from_bd() -> set[dict]:
     path = os.path.join(sys.path[0], _BD)
-    with open(path, 'rb') as file:
-        res = pickle.load(file)
-    logger.info('Чтение из БД')
-    return res
+    try:
+        with open(path, 'rb') as file:
+            res = pickle.load(file)
+        logger.info('Чтение из БД')
+        return res
+    except FileNotFoundError:
+        print("Загрузить БД пользователей не возможно. Отсутствует файл datebase\\bd.pickle.\nПрекращение работы")
+        write_to_bd()
 
+def create_bd(path) -> None:
+    users_bd = {0:UserBD()}
+    # # Создаём папку
+    # os.makedirs(os.path.dirname(path), exist_ok=True)  # exist_ok=True — не вызывает ошибку, если папка уже есть
+    with open(path, 'wb') as file:
+        pickle.dump(users_bd, file)
+    logger.info('Создание файла БД')
 
 # Инициализируем "базу данных"
+path = os.path.join(sys.path[0], _BD)
+if not os.path.exists(path):
+    create_bd(path)
 users_bd: dict[int, UserBD] = read_from_bd()
 print(users_bd)
